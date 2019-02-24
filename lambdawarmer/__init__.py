e85e0cc553ecb5fcd07c22589537b05514236d49
robhowley <howley.robert@gmail.com>
2019-02-24 00:03:28 -0500

    warmer decorator and test

---
+++ lambdawarmer/__init__.py
@@ -0,0 +1,100 @@
+
+import os
+import json
+import time
+import boto3
+import logging
+import functools
+from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor
+
+
+LAMBDA_INFO = {
+    'is_warm': False,
+    'name': os.environ.get('AWS_LAMBDA_FUNCTION_NAME', 'FAILED_TO_RETRIEVE_LAMBDA_NAME')
+}
+
+
+def invoke_lambda(parameter_tuple):
+    lambda_client, function_name, invocation_type_string, payload = parameter_tuple
+    start = datetime.utcnow()
+    thread_int = payload['__WARMER_INVOCATION__']
+    print('start ', thread_int)
+    lambda_client.invoke(
+        FunctionName=function_name,
+        InvocationType=invocation_type_string,
+        Payload=json.dumps(payload)
+    )
+    print('runtime for ', thread_int, (datetime.utcnow() - start).total_seconds())
+
+
+def warmer(configuration=None, lambda_client=None, logger=None):
+    def decorator(f):
+        @functools.wraps(f)
+        def wrapped_func(event, context, *args, **kwargs):
+            config = dict(
+                flag='warmer',                          # default event key for flag indicating a warmer invocation
+                concurrency='concurrency',              # default event key for flag indicating a test invocation
+                test='test',                            # default event key for concurrency setting
+                correlation_id=context.aws_request_id,  # default the shared id to the request id of source lamdba
+                delay=75,                               # default the delay to 75ms
+                **(configuration or {})
+            )
+
+            warmer_fan_out(event, config=config, lambda_client=lambda_client, logger=logger)
+
+            return f(event, context, *args, **kwargs)
+        return wrapped_func
+    return decorator
+
+
+def warmer_fan_out(event, config=None, lambda_client=None, logger=None):
+
+    logger = logger or logging.getLogger(__name__)
+
+    state_at_invocation = LAMBDA_INFO['is_warm']
+    LAMBDA_INFO['is_warm'] = True
+
+    if event.get(config['flag']):
+        concurrency = max(event.get(config['concurrency']), 1)
+        invoke_count = event.get('__WARMER_INVOCATION__') or 1
+        invoke_total = event.get('__WARMER_CONCURRENCY__') or concurrency
+        correlation_id = event.get('__WARMER_CORRELATION_ID__') or config['correlation_id']
+
+        logger.info(dict(
+            action='warmer',
+            function=LAMBDA_INFO['name'],
+            instance_id=config['correlation_id'],
+            correlation_id=correlation_id,
+            count=invoke_count,
+            concurrency=invoke_total,
+            is_warm=state_at_invocation
+        ))
+
+        LAMBDA_INFO['is_warm'] = True
+
+        if concurrency > 1 and not event.get(config['test']):
+            with ThreadPoolExecutor(max_workers=concurrency) as executor:
+                lambda_client = lambda_client or boto3.client('lambda')
+
+                base_payload = {
+                    config['flag']: True,
+                    '__WARMER_CONCURRENCY__': concurrency,
+                    '__WARMER_CORRELATION_ID__': correlation_id
+                }
+
+                param_iterables = [
+                    (
+                        lambda_client,
+                        LAMBDA_INFO['name'],
+                        'RequestResponse' if i == (concurrency - 1) else 'Event',
+                        dict(base_payload, __WARMER_INVOCATION__=(i + 1))
+                    )
+                    for i in range(1, concurrency)
+                ]
+
+                executor.map(invoke_lambda, param_iterables)
+
+        elif invoke_count > 1:
+            # if you don't delay, you might just get a reused container
+            time.sleep(config['delay'] / 1000.0)
